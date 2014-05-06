ac4dcae23a06038ec56078397a2e72a9112a3524
Daniel Romero <infoslack@gmail.com>
2014-05-06 13:10:19 -0300

    configure mina for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,16 @@
+require "mina/bundler"
 require "mina/git"
 
 set :domain, "infoslack.com"
 set :user, "deploy"
 set :deploy_to, "/var/www/infoslack"
 set :repository, "https://github.com/infoslack/infoslack-jekyll.git"
-set :branch, "deploy"
+set :branch, "master"
 
 task :deploy do
   deploy do
     invoke :"git:clone"
+    invoke :"bundle:install"
+    queue "#{bundle_prefix} jekyll --lsi build"
   end
 end
