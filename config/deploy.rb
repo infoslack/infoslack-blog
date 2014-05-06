da2e6e59fcd4e356e57d89d2efa8dd93468e7f8c
Daniel Romero <infoslack@gmail.com>
2014-05-06 12:29:11 -0300

    Revert "config capistrano for deploy"
    This reverts commit 74ba2ea57eb14fa5f8f18f1cab0a719c80020f9c.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,13 @@
-lock "3.2.1"
+require "mina/git"
 
-set :application, "infoslack.com"
-set :repository, "./_deploy"
-set :scm, :none
-
-set :use_sudo, false
+set :domain, "infoslack.com"
 set :user, "deploy"
-set :port, 22
-
 set :deploy_to, "/var/www/infoslack"
-set :deploy_via, :copy
-set :copy_strategy, :export
-set :keep_releases, 5
+set :repository, "https://github.com/infoslack/infoslack-jekyll.git"
+set :branch, "deploy"
 
-namespace :deploy do
-  [:start, :stop, :restart, :finalize_update].each do |t|
-    desc "#{t} task is a no-op with jekyll"
-    task t, roles: :app do; end
+task :deploy do
+  deploy do
+    invoke :"git:clone"
   end
 end
--- config/deploy/production.rb
+++
@@ -1,5 +0,0 @@
-set :stage, :production
-
-role :app, %w{deploy@infoslack.com}
-role :web, %w{deploy@infoslack.com}
-role :db,  %w{deploy@infoslack.com}
