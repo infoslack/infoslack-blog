751b8fd3e22a3b95fdcf66d3b5b9ea6dc2375287
Daniel Romero <infoslack@gmail.com>
2014-11-20 23:50:25 -0300

    config shared links

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,10 +6,11 @@ set :user, "deploy"
 set :deploy_to, "/var/www/infoslack"
 set :repository, "https://github.com/infoslack/infoslack-jekyll.git"
 set :branch, "master"
-set :shared_paths, ["_site/rubyconf"]
+set :shared_paths, ["_site/rubyconf", "_site/inscricao.html"]
 
 task :setup => :environment do
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/_site/rubyconf"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/_site/inscricao.html"]
 end
 
 task :deploy => :environment do
