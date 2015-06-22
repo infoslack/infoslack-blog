3a14cb8b83e6d12a4ec7d4d9fc6f20b56f778d76
Daniel Romero <infoslack@gmail.com>
2015-06-22 10:35:29 -0300

    add talks and update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,10 +6,9 @@ set :user, "deploy"
 set :deploy_to, "/var/www/infoslack"
 set :repository, "https://github.com/infoslack/infoslack-jekyll.git"
 set :branch, "master"
-set :shared_paths, ["_site/rubyconf", "_site/docker", "_site/inscricao.html", "_site/workshops", "_site/ansible"]
+set :shared_paths, ["_site/metasploit", "_site/docker", "_site/inscricao.html", "_site/workshops", "_site/ansible"]
 
 task :setup => :environment do
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/_site/rubyconf"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/_site/inscricao.html"]
 end
 
---
+++ talks/index.md
@@ -0,0 +1,15 @@
+---
+layout: page
+title: Talks
+keywords: infoslack, linux, software, developer, engineer, hacker, devops, palestras
+---
+
+### 2015
+
+- [16° GURU-CE - Automatize o provisionamento de servidores com Ansible](http://infoslack.com/ansible)
+- [15° GURU-CE - Segurança on Rails](https://speakerdeck.com/infoslack/seguranca-on-rails)
+- [14° GURU-CE - Conhecendo o Docker](http://infoslack.com/docker)
+
+### 2014
+
+- [RubyConf BR 2014 - Testando a segurança de suas aplicações com Metasploit framework](http://infoslack.com/metasploit)
